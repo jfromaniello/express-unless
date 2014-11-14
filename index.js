794fa511613a636119273a149c53d44e7a30a3b3
Sasha Mile Rudan <mprinc@gmail.com>
2014-11-14 23:22:29 +0100

    Added support for chaining unless 	    	.unless({path: ['/test', '/fobo']}) 	    	.unless({path: {p: '/nada', m: ['PUT', 'GET']}}) 	    	.unless({method: ['OPTIONS']});
    Added support for path describing as object
    .unless({
    	      path: [
     	        '/milica',
    	        {p: '/nada', m: 'OPTIONS'},
    	        {p: '/mile', m: ['DELETE', 'PUT']}
    	      ]
    	    });

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 var URL = require('url');
 
-module.exports = function (options) {
+var middleware = function (options) {
   var parent = this;
+  //console.log("[unless] options = %s, this = %s", JSON.stringify(options), this);
 
   var opts = typeof options === 'function' ? {custom: options} : options;
 
-  return function (req, res, next) {
+  var unless = function (req, res, next) {
     var url = URL.parse(req.originalUrl || req.url || '', true);
 
     var skip = false;
@@ -17,10 +18,23 @@ module.exports = function (options) {
     var paths = !opts.path || Array.isArray(opts.path) ?
                 opts.path : [opts.path];
 
+    //console.log("[unless] paths: %s", JSON.stringify(paths));
     if (paths) {
-      skip = skip || paths.some(function (p) {
-        return (typeof p === 'string' && p === url.pathname) ||
-               (p instanceof RegExp && !!p.exec(url.pathname));
+      skip = skip || paths.some(function (path) {
+    	if(path instanceof Object && !(path instanceof RegExp)){
+    		var p = path.p;
+    		var m = path.m;
+    	}else{
+        	var p = path;
+    	}
+        //console.log("[unless] path: %s, req.method: %s, m: %s, Array.isArray(m): %s, !!~m.indexOf(req.method): %s", p, req.method, m, Array.isArray(m), m && !!~m.indexOf(req.method));
+        return (
+          (typeof p === 'string' && p === url.pathname)
+          || (p instanceof RegExp && !!p.exec(url.pathname)))
+        && (!m
+          || (typeof m === 'string' && m === req.method)
+          || (Array.isArray(m) && !!~m.indexOf(req.method))
+          );
       });
     }
 
@@ -33,6 +47,8 @@ module.exports = function (options) {
       });
     }
 
+    // mprinc
+    // It has to be fast, avoid wrapping in Array
     var methods = !opts.method || Array.isArray(opts.method) ?
                   opts.method : [opts.method];
 
@@ -41,9 +57,16 @@ module.exports = function (options) {
     }
 
     if (skip) {
+    	//console.log("[express-jwt:unless] skipping for: req.method = %s, req.url = %s", 
+    	//	req.method, req.url);
       return next();
     }
 
     parent(req, res, next);
   };
+  
+  unless.unless = middleware; // fluid interface, chainable
+  return unless;
 };
+
+module.exports = middleware;
